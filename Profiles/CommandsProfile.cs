6b8231a58fc90754c91065472380a4c300057123
EToplica <endrit.toplica@outlook.com>
2021-07-15 16:47:20 +0200

    FirstCommit

---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using Commander.Dtos;
+using Commander.Model;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            //Source -> Target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+            CreateMap<Command, CommandUpdateDto>();
+            
+        }
+    }
+}
